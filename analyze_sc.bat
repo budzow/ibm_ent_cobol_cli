6aec6d93e98fa961ac3a18f84dcbe41d6d80d906
Michal Budzowski <michal.budzowski@sonarsoruce.com>
2020-02-11 15:01:21 +0100

    Initial commit

---
+++ analyze_sc.bat
@@ -0,0 +1,1 @@
+sonar-scanner -Dsonar.sources=. -Dsonar.projectKey=ibm_ent_cobol_cli -Dsonar.organization=budzow-github -Dsonar.login=e5990ff3d41ff9d6be8cd6345fee7cef1485f3c0 -Dsonar.host.url=http://sonarcloud.io -Dsonar.projectVersion=1
