9743a60e7efc15acfa34c22da5f3f2534760a219
Michal Budzowski <michal.budzowski@sonarsoruce.com>
2020-02-11 15:08:09 +0100

    remove token

--- analyze_sc.bat
+++ analyze_sc.bat
@@ -1,1 +1,4 @@
-sonar-scanner -Dsonar.sources=. -Dsonar.projectKey=ibm_ent_cobol_cli -Dsonar.organization=budzow-github -Dsonar.login=e5990ff3d41ff9d6be8cd6345fee7cef1485f3c0 -Dsonar.host.url=http://sonarcloud.io -Dsonar.projectVersion=1
+rem sonar-scanner -Dsonar.sources=. -Dsonar.projectKey=ibm_ent_cobol_cli -Dsonar.organization=budzow-github -Dsonar.login=e5990ff3d41ff9d6be8cd6345fee7cef1485f3c0 -Dsonar.host.url=http://sonarcloud.io -Dsonar.projectVersion=1
+
+set /p token=SonarCloud token?
+sonar-scanner -Dsonar.sources=. -Dsonar.projectKey=ibm_ent_cobol_cli -Dsonar.organization=budzow-github -Dsonar.login=%token% -Dsonar.host.url=http://sonarcloud.io -Dsonar.projectVersion=1
